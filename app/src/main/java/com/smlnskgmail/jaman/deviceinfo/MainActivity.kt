bed3c2f8399e5bc3d06462531daa625c974da1b9
fartem <jaman.smlnsk@gmail.com>
2019-07-20 07:44:56 +0300

    2019-07-20 Version 0.1.0: Initial commit

---
+++ app/src/main/java/com/smlnskgmail/jaman/deviceinfo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.smlnskgmail.jaman.deviceinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}

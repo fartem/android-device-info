165314407ddae5628a3f3df54da34137f649767b
fartem <jaman.smlnsk@gmail.com>
2019-08-23 08:59:34 +0300

    2019-08-23 Version 0.5.0: Added info about CPU and updated Gradle plugin

--- app/src/main/java/com/smlnskgmail/jaman/deviceinfo/info/DeviceCPU.kt
+++ app/src/main/java/com/smlnskgmail/jaman/deviceinfo/info/DeviceCPU.kt
@@ -1,5 +1,70 @@
 package com.smlnskgmail.jaman.deviceinfo.info
 
+import java.io.File
+import java.io.FileFilter
+import java.util.regex.Pattern
+
+
 class DeviceCPU {
+
+    fun cores() = Runtime.getRuntime().availableProcessors()
+
+    fun minimumFreq(): Int {
+        val cpus = cpus()
+        var minimumFreq = 0
+
+        if (cpus != null) {
+            for (cpu in cpus) {
+                val minCpuFreq = cat("${cpu.absolutePath}/cpufreq/cpuinfo_min_freq")
+                val minCpuFreqValue = minCpuFreq.toInt()
+
+                if (minCpuFreqValue < minimumFreq || minimumFreq == 0) {
+                    minimumFreq = minCpuFreqValue
+                }
+            }
+        }
+        return minimumFreq
+    }
+
+    fun maximumFreq(): Int {
+        val cpus = cpus()
+        var maximumFreq = 0
+
+        if (cpus != null) {
+            for (cpu in cpus) {
+                val maxCpuFreq = cat("${cpu.absolutePath}/cpufreq/cpuinfo_max_freq")
+                val maxCpuFreqValue = maxCpuFreq.toInt()
+
+                if (maxCpuFreqValue < maximumFreq || maximumFreq == 0) {
+                    maximumFreq = maxCpuFreqValue
+                }
+            }
+        }
+        return maximumFreq
+    }
+
+    private fun cat(path: String): String {
+        val command = arrayOf("cat", path)
+        val process = ProcessBuilder(*command).start()
+
+        var result = ""
+
+        process.inputStream.bufferedReader().use {
+            result = it.readText()
+        }
+
+        return result
+    }
+
+    private fun cpus(): Array<File>? {
+        class CpuFilter : FileFilter {
+            override fun accept(pathname: File): Boolean {
+                return Pattern.matches("cpu[0-9]+", pathname.name)
+            }
+        }
+
+        val dir = File("/sys/devices/system/cpu/")
+        return dir.listFiles(CpuFilter())
+    }
 
 }
